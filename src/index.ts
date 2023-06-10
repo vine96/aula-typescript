555996e4253585e9d5d836d3494e854f64897d9c
vine96 <viniciuspecci@hotmail.com>
2023-06-10 14:44:23 -0300

    Typescript types and type lists

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
+// Tipos básicos
 let age: number = 5;
+const firstName: string = 'Vinícius';
+const isValid: boolean = true;
+let idk: any = 5;
+
+idk = '10';
+idk = true;
+
+// Listas com typescript
+const ids: number[] = [1, 2, 3, 4, 5];
+const booleans: boolean[] = [true, false, true, true];
+const strings: string[] = ['Azul', 'Vermelho', 'Amarelo'];
 
 console.log(age);
